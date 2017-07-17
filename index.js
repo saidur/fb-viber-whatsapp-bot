4f35ccb9bbe3e8b206a1f8b6439c70e9b7522b8f
root <root@ubuntu-512mb-blr1-01.localdomain>
2017-07-17 08:10:46 +0000

    modify index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');  
 var bodyParser = require('body-parser');  
 var request = require('request');  
@@ -5,6 +7,109 @@ var app = express();
 var useragent = require('express-useragent');
 var path    = require("path");
 var stringSearcher = require('string-search');
+/* 
+
+add viber 
+*/
+
+const ViberBot = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
+const TextMessage = require('viber-bot').Message.Text;
+
+const winston = require('winston');
+const toYAML = require('winston-console-formatter');
+
+
+function createLogger() {
+    const logger = new winston.Logger({
+        level: "debug" // We recommend using the debug level for development
+    });
+
+    logger.add(winston.transports.Console, toYAML.config());
+    return logger;
+}
+
+
+function say(response, message) {
+    response.send(new TextMessage(message));
+}
+
+
+function checkUrlAvailability(botResponse, urlToCheck) {
+
+    if (urlToCheck === '') {
+        say(botResponse, 'I need a URL to check');
+        return;
+    }
+
+    say(botResponse, 'One second...Let me check!');
+
+    var url = urlToCheck.replace(/^http:\/\//, '');
+    request('http://isup.me/' + url, function(error, requestResponse, body) {
+        if (error || requestResponse.statusCode !== 200) {
+            say(botResponse, 'Something is wrong with isup.me.');
+            return;
+        }
+
+        if (!error && requestResponse.statusCode === 200) {
+            if (body.search('is up') !== -1) {
+                say(botResponse, 'Hooray! ' + urlToCheck + '. looks good to me.');
+            } else if (body.search('Huh') !== -1) {
+                say(botResponse, 'Hmmmmm ' + urlToCheck + '. does not look like a website to me. Typo? please follow the format `test.com`');
+            } else if (body.search('down from here') !== -1) {
+                say(botResponse, 'Oh no! ' + urlToCheck + '. is broken.');
+            } else {
+                say(botResponse, 'Snap...Something is wrong with isup.me.');
+            }
+        }
+    })
+}
+
+
+const logger = createLogger();
+const VIBER_PUBLIC_ACCOUNT_ACCESS_TOKEN_KEY ="464b4b09d9312d68-f40d732c7a251e8c-223ffae9b84c06fe";
+
+if (!VIBER_PUBLIC_ACCOUNT_ACCESS_TOKEN_KEY) {
+    logger.debug('Could not find the Viber Public Account access token key in your environment variable. Please make sure you followed readme guide.');
+    return;
+}
+
+// Creating the bot with access token, name and avatar
+const bot = new ViberBot(logger, {
+    authToken: VIBER_PUBLIC_ACCOUNT_ACCESS_TOKEN_KEY, // Learn how to get your access token at developers.viber.com
+    name: "Is It Up",
+    avatar: "https://raw.githubusercontent.com/devrelv/drop/master/151-icon.png" // Just a placeholder avatar to display the user
+});
+
+// The user will get those messages on first registration
+bot.onSubscribe(response => {
+    say(response, `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me if a web site is down for everyone or just you. Just send me a name of a website and I'll do the rest!`);
+});
+
+bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+    // This sample bot can answer only text messages, let's make sure the user is aware of that.
+    if (!(message instanceof TextMessage)) {
+        say(response, 'Sorry. I can only understand text messages.');
+    }
+});
+
+bot.onTextMessage(/./, (message, response) => {
+    checkUrlAvailability(response, message.text);
+});
+
+const WEB_URL='https://botmela.samuraigeeks.net/';
+
+if (process.env.NOW_URL || process.env.HEROKU_URL || WEB_URL) {
+    const http = require('http');
+    const port = process.env.PORT || 8080;
+
+    http.createServer(bot.middleware()).listen(port, () => bot.setWebhook(process.env.NOW_URL || process.env.HEROKU_URL||WEB_URL));
+} else {
+    logger.debug('Could not find the now.sh/Heroku environment variables. Please make sure you followed readme guide.');
+}
+
+
+
 // this is for chakribot
 //const PAGE_ACCESS_TOKEN = "EAAEnw2c9cIsBAELqZAzfZCi7bbUctFplk8uQXGPBeNeEReqoZBnlM45atX68e8iStouCQGzBLPZCHoZBJOdGMiwk9HoTvueu7dZB8Krrx36WKtYfmhhF8ZAsLNWmKv0BSZArPvtAvZB0cOiaw7c8mXw2Aasbd7o8ZBfF376xvTcxNl5AZDZD"
 const PAGE_ACCESS_TOKEN = "EAAEnw2c9cIsBAD1ZB7Hs08wo2f8kpanSdyfVkERDN7GZAhVfEZBuQi9ZC7ntwjz8ZCV05UrdnF9RiOPCH5ZADvkL7TZBNTs5oh6EtkSWikvbjI6j1aXEOSKIb4Kgc4iXggMP2PSXecAumGIoZAPHrUU7MV5oaevZBltto8TXZBneeKuAZDZD"
